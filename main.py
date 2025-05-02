6e4bc9ba584abae662f718cc5d0fe8952eb5e064
SashaKarev <sashka.kariev@bk.ru>
2025-05-02 15:23:54 +0300

    Создан каркас

---
+++ main.py
@@ -0,0 +1,40 @@
+import pickle
+
+
+def main():
+    # Главный опросник пользователя
+    # Список команд: 1 - Вывести все позиции
+    # 2 - Добавить позицию
+    # 3 - Удалить позицию
+    # 4 - Редактировать позицию
+    pass
+
+
+def select_all_position():
+    pass
+
+
+def add_position():
+    pass
+
+
+def delete_position(number_position):
+    pass
+
+
+def edit_position(number_position):
+    pass
+
+
+# функция для сериализации данных в файл
+def add_file(number, name, price, number_rack, number_cell):
+    pass
+
+
+# функция для десериализации данных из файла
+def load_from_file():
+    pass
+
+
+if __name__ == '__main__':
+    main()
